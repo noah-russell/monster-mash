81378a67878bf0e00338ef324a81b81aa48b63ff
noah-russell-roa-23 <143241915+noah-russell@users.noreply.github.com>
2023-12-01 10:18:06 +1300

    Started setting up canvas

---
+++ client/components/DrawingZone.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Canvas } from './Canvas'
+import { ClearCanvasButton } from './ClearCanvasButton'
+
+function DrawingZone() {
+  return (
+    <>
+      <Canvas />
+      <ClearCanvasButton />
+    </>
+  )
+}
+
+export default DrawingZone
