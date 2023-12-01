01932708a2ec9c457f290eab54c9fdad9c5e0be5
cody-ilg <143582504+cody-ilg@users.noreply.github.com>
2023-12-01 14:56:01 +1300

    added outlines for components and CSS classnames

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -2,7 +2,7 @@ function Header() {
   return (
     <>
       <div className="header">
-        <h1>Monster Mash</h1>
+        <h1 className="header-text">Monster Mash</h1>
         <p>Create your creepiest creation!</p>
       </div>
     </>
