4cece4a723dbc7a4611f8cd178b163586e0078cb
rose northey <roseAnorthey@gmail.com>
2023-12-03 07:14:40 +1300

    speech bubble styling

--- client/components/Colin.tsx
+++ client/components/Colin.tsx
@@ -1,10 +1,40 @@
 function Colin() {
+  const artistOne = "Michael"
+  const artistTwo = "Gaby"
+
+  const colinTips=[
+    {
+      tip1: `${artistTwo}, close your eyes!`,
+      tip2: `${artistOne}, draw me the top of the monster and then hit done when finished!`,
+      hint: `Make sure you draw right the the bottom of the white canvas`
+    },
+    {
+      tip1: `${artistOne}, close your eyes!`,
+      tip2: `${artistTwo}, draw me the bottom of the monster and then hit done when finished!`,
+      hint: `Make sure you draw right to the top of where Artist One's lines finish` 
+    }
+  ]
+
+  const gameState = 0;
+  const currentColinTip = colinTips[gameState]
+
+
   return (
+    <>
+    <div className="speech-bubble">
+      <p>{currentColinTip.tip1}</p>
+      <br/>
+      <p>{currentColinTip.tip2}</p>
+      <br/>
+      <em>{currentColinTip.hint}</em>
+    </div>
+
       <img
         className="colin-image"
         src="client/public/colin.png"
         alt="colin"
       />
+    </>
   )
 }
 export default Colin
