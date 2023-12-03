1161371f3a556d46c60e951a2742d6f424b941ca
noah-russell-roa-23 <143241915+noah-russell@users.noreply.github.com>
2023-12-03 13:12:51 +1300

    position tools in square

--- client/components/PlayHotSeat.tsx
+++ client/components/PlayHotSeat.tsx
@@ -14,11 +14,16 @@ function PlayHotSeat() {
           </div>
         </div>
         <div className="controls vflex">
+          <img className="controls-img" src="client/public/toolbar.png" />
+
           <div className="colour-controls">
-            <img src='client/public/pencil.png'/>
-            <img src='client/public/rubber.png'/>
+            <img src="client/public/pencil.png" />
+            <img src="client/public/rubber.png" />
           </div>
-          <button><p>done</p></button>
+
+          <button>
+            <p>done</p>
+          </button>
         </div>
       </div>
     </>
