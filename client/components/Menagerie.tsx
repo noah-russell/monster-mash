ebcea9ec07c2a5df00a75382dace833dfb4580f3
IggyNash <143356841+IggyNash@users.noreply.github.com>
2023-12-04 16:18:14 +1300

    Menagerie Component with imported monster data

---
+++ client/components/Menagerie.tsx
@@ -0,0 +1,18 @@
+import { getAllMonsters } from '../apiFunctions'
+import { useQuery } from '@tanstack/react-query'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+
+function Menagerie() {
+  const {
+    data: monsters,
+    isLoading,
+    isError,
+    error,
+  } = useQuery(['monsters'], getAllMonsters, {})
+  console.log(monsters)
+  return <></>
+}
+
+export default Menagerie
