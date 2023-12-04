240079c088bf2a799cce5255fd7247f805c68c35
noah-russell-roa-23 <143241915+noah-russell@users.noreply.github.com>
2023-12-05 10:57:03 +1300

    added cs api function

--- client/apiFunctions.ts
+++ client/apiFunctions.ts
@@ -2,14 +2,16 @@ import request from 'superagent'
 import { Monster, NewMonster } from '../models/monster-models'
 import { response } from 'express'
 
-export async function getAllMonsters(){
+export async function getAllMonsters() {
   const monsters = await request.get('/api/v1/gallery')
   return monsters.body
 }
 
+export async function getMonsterById(id: number) {
+  const monster = await request.get(`/api/v1/monster/${id}`)
+  return monster.body
+}
 
-export async function uploadMonster(formData: FormData){
-  const response = await request
-  .post('api/v1/add')
-  .send(formData)
+export async function uploadMonster(formData: FormData) {
+  const response = await request.post('api/v1/add').send(formData)
 }
