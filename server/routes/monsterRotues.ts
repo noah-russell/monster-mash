28ad1fcc7dd99f86a7fe7d3e18dabcedcfa7e3f9
noah-russell-roa-23 <143241915+noah-russell@users.noreply.github.com>
2023-12-05 10:52:30 +1300

    server side routes set up

--- server/routes/monsterRotues.ts
+++ server/routes/monsterRotues.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import * as db from '../db/monsterDb.ts'
-import multer from 'multer';
+import multer from 'multer'
 
 // import * as db from '../db/db.ts'
 // '/api/v1'
@@ -9,39 +9,40 @@ const router = Router()
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    return cb(null, 'monsters/');
+    return cb(null, 'monsters/')
   },
   filename: function (req, file, cb) {
-    return cb(null, `${Date.now()}.png`);
+    return cb(null, `${Date.now()}.png`)
   },
-});
+})
 
-const upload = multer({ storage });
+const upload = multer({ storage })
 
 // upload new image
 router.post('/add', upload.single('file'), async (req, res) => {
   console.log('router is being accessed')
   try {
     if (!req.file) {
-      res.status(400).json({ error: 'No file uploaded' });
-      return;
+      res.status(400).json({ error: 'No file uploaded' })
+      return
     }
     const monsterDataForDb = {
       monster_name: req.body.monster_name,
       top_artist: req.body.top_artist,
       bottom_artist: req.body.bottom_artist,
       image_url: `monsters/${req.file.filename}`,
-    };
+    }
     console.log('monster Data For Db')
     console.log(monsterDataForDb)
-    const newMonsterId = await db.addMonster(monsterDataForDb);
-    res.status(200).json({ newMonsterId });
+    const newMonsterId = await db.addMonster(monsterDataForDb)
+    res.status(200).json({ newMonsterId })
   } catch (error) {
-    console.error('Error in POST /api/v1/artworks/upload:', error);
-    res.status(500).json({ error: 'Internal Server Error', details: error.message });
+    console.error('Error in POST /api/v1/artworks/upload:', error)
+    res
+      .status(500)
+      .json({ error: 'Internal Server Error', details: error.message })
   }
-});
-
+})
 
 router.get('/gallery', async (req, res) => {
   try {
@@ -54,8 +55,21 @@ router.get('/gallery', async (req, res) => {
   }
 })
 
+router.get('/monster/:id', async (req, res) => {
+  const id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.status(404).json({ error: 'id must be a number' })
+    return
+  }
 
+  try {
+    const monster = await db.getMonsterById(id)
 
-
+    res.json(monster)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ message: 'RARGH Server Error getMonsterById' })
+  }
+})
 
 export default router
