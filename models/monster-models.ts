187a726c007af4898f74775f056b5426a03a3f15
IggyNash <143356841+IggyNash@users.noreply.github.com>
2023-12-04 14:22:42 +1300

    monster db fuction getAllMonsters

---
+++ models/monster-models.ts
@@ -0,0 +1,15 @@
+export interface NewMonster {
+  monster_name: string
+  top_artist: string
+  bottom_artist: string
+  image_url: string
+}
+
+export interface Monster {
+  monster_name: string
+  top_artist: string
+  bottom_artist: string
+  image_url: string
+  id: number
+  date_created: Date
+}
