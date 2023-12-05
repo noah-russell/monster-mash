2dbd6317aceea9a07aa1169a72d513777901127d
IggyNash <143356841+IggyNash@users.noreply.github.com>
2023-12-05 17:18:58 +1300

    refactor code

--- client/components/SubmitMonsterForm.tsx
+++ client/components/SubmitMonsterForm.tsx
@@ -6,44 +6,26 @@ import { useNavigate } from 'react-router-dom'
 
 function SubmitMonsterForm({ topArtist, bottomArtist, gameState }) {
   const { canvasRef, saveCanvasAsImage } = useCanvas()
-  const formData = new FormData()
-
   const navigate = useNavigate()
-
+  const queryClient = useQueryClient()
   const [monsterName, setMonsterName] = useState('')
+  const formData = new FormData()
 
   const handleMonsterNameChange = (
     event: React.ChangeEvent<HTMLFormElement>,
   ) => {
     setMonsterName(event.target.value)
   }
-  console.log(monsterName)
-  // ____________________________________________________________
-  // TO BE REMOVED LATER
 
-  const hardCodedMonsterName = 'lemon-breath'
-  // Destructure hard coded data so that the code below will
-  // be the same when we add the real values
-
-  const top_artist = topArtist
-  const bottom_artist = bottomArtist
-  const monster_name = hardCodedMonsterName
-
-  // ____________________________________________________________
-  // begin mutation
-
-  const queryClient = useQueryClient()
-
+  
   const generateFormDataWithBlob = async () => {
     const dataURL = canvasRef.current.toDataURL('image/png')
     const blobData = await fetch(dataURL).then((res) => res.blob())
     formData.append('file', blobData)
   }
-
   const uploadMonsterMutation = useMutation({
     mutationFn: uploadMonster,
     onSuccess: async () => {
-      console.log('invalidate queries loop for add')
       queryClient.invalidateQueries({ queryKey: ['monsters'] })
     },
   })
@@ -54,11 +36,10 @@ function SubmitMonsterForm({ topArtist, bottomArtist, gameState }) {
     event.preventDefault()
     await generateFormDataWithBlob()
     formData.append('monster_name', monsterName)
-    formData.append('top_artist', top_artist)
-    formData.append('bottom_artist', bottom_artist)
+    formData.append('top_artist', topArtist)
+    formData.append('bottom_artist', bottomArtist)
     try {
       uploadMonsterMutation.mutate(formData)
-      console.log('add monster mutation successful on component')
       navigate(`/menagerie`)
     } catch (error) {
       console.error('An error occurred during uploading:', error)
