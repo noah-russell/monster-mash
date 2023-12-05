29346ba171906dab762ce9fc90842648002cbfd9
IggyNash <143356841+IggyNash@users.noreply.github.com>
2023-12-05 20:27:01 +1300

    made colin welcome row good

--- client/components/Welcome.tsx
+++ client/components/Welcome.tsx
@@ -22,18 +22,20 @@ function Welcome({ setIsWelcome, setTopArtist, setBottomArtist }) {
 
   return (
     <div className="welcome-container">
-      <div className="welcome-flexbox hflex">
+      <div className="colin-welcome">
         <img
-          src="client/public/colin.png"
+          src="client/public/colinwelcome.png"
           alt="colin waving"
           className="colin"
         />
         <div className="welcome-bubble">
-          <h2>
-            Greetings minion! I need you to make a scary new monster... Make
-            sure you have a friend with you, it takes two to make a truly
-            terrifying beast
-          </h2>
+          <div >
+            <h2>
+              Greetings minion! I need you to make a scary new monster... Make
+              sure you have a friend with you, it takes two to make a truly
+              terrifying beast
+            </h2>
+          </div>
         </div>
       </div>
 
