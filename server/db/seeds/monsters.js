f6f478eeb42d1d6926f09f169e43d328512fe913
student <student@pop-os.localdomain>
2024-01-16 13:43:39 +1300

    changed migration img file path

--- server/db/seeds/monsters.js
+++ server/db/seeds/monsters.js
@@ -12,7 +12,7 @@ export async function seed(knex) {
       top_artist: 'Patrice',
       bottom_artist: 'Rose',
       date_created: new Date(),
-      image_url: 'monsters/Clause.png',
+      image_url: '/Clause.png',
     },
     {
       id: 2,
@@ -20,7 +20,7 @@ export async function seed(knex) {
       top_artist: 'Iggy',
       bottom_artist: 'Patrice',
       date_created: new Date(),
-      image_url: 'monsters/Gorgeous.png',
+      image_url: '/Gorgeous.png',
     },
     {
       id: 3,
@@ -28,7 +28,7 @@ export async function seed(knex) {
       top_artist: 'Iggy',
       bottom_artist: 'Blue',
       date_created: new Date(),
-      image_url: 'monsters/Iggy.png',
+      image_url: '/Iggy.png',
     },
 
     {
@@ -37,7 +37,7 @@ export async function seed(knex) {
       top_artist: 'Murphy',
       bottom_artist: 'Raidon',
       date_created: new Date(),
-      image_url: 'monsters/Murp.png',
+      image_url: '/Murp.png',
     },
     {
       id: 5,
@@ -45,7 +45,7 @@ export async function seed(knex) {
       top_artist: 'Mosh',
       bottom_artist: 'Scarah',
       date_created: new Date(),
-      image_url: 'monsters/Schmaz.png',
+      image_url: '/Schmaz.png',
     },
     {
       id: 6,
@@ -53,7 +53,7 @@ export async function seed(knex) {
       top_artist: 'Scarah',
       bottom_artist: 'lizardbreath',
       date_created: new Date(),
-      image_url: 'monsters/Vesvius Tripe.png',
+      image_url: '/Vesvius Tripe.png',
     },
     {
       id: 7,
@@ -61,7 +61,7 @@ export async function seed(knex) {
       top_artist: 'winner',
       bottom_artist: 'loooooseeeeer',
       date_created: new Date(),
-      image_url: 'monsters/SoleSeeker.png',
+      image_url: '/SoleSeeker.png',
     },
     {
       id: 8,
@@ -69,7 +69,7 @@ export async function seed(knex) {
       top_artist: 'Deeevin',
       bottom_artist: 'Geb',
       date_created: new Date(),
-      image_url: 'monsters/Cosy Sammy.png',
+      image_url: '/Cosy Sammy.png',
     },
     {
       id: 9,
@@ -77,7 +77,7 @@ export async function seed(knex) {
       top_artist: 'Rose Juice',
       bottom_artist: 'Patrice',
       date_created: new Date(),
-      image_url: 'monsters/Picasso.png',
+      image_url: '/Picasso.png',
     },
     {
       id: 10,
@@ -85,7 +85,7 @@ export async function seed(knex) {
       top_artist: 'binker',
       bottom_artist: 'chainsaw',
       date_created: new Date(),
-      image_url: 'monsters/saw gangan.png',
+      image_url: '/saw gangan.png',
     },
 
 
